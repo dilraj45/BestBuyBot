948b0b673b89c735af20bed88bdfe50699b5ecc9
Dilraj Singh <dilrasin@amazon.com>
2021-05-01 01:34:59 +0530

    Created webpack package for chrome extension

---
+++ src/background/init.ts
@@ -0,0 +1,6 @@
+console.log("Bootstrapping background application");
+
+chrome.runtime.onInstalled.addListener(() => {
+    console.log("Welcome to BestBuyBot user community. Hope you'll enjoy this.")
+});
+
